cf619e6a6c92bb21ff212b071a7f076787ee9fa5
ngcx2237 <contact.lamn@icloud.com>
2026-01-07 02:21:10 +0700

    fix: :bug: update channel model request structure in SyncLLMTask

--- internal/task/sync_llm.go
+++ internal/task/sync_llm.go
@@ -51,7 +51,11 @@ func SyncLLMTask() {
 
 		// 更新渠道模型
 		channel.Model = newModelStr
-		if err := op.ChannelUpdate(&channel, ctx); err != nil {
+		updateReq := &model.ChannelUpdateRequest{
+			ID:    channel.ID,
+			Model: &newModelStr,
+		}
+		if _, err := op.ChannelUpdate(updateReq, ctx); err != nil {
 			log.Errorf("failed to update channel %s: %v", channel.Name, err)
 			continue
 		}
